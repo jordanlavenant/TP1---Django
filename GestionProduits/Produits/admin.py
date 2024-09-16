e67e0ed3bc6f5680334df31f382a7f7f919583fc
jordanlavenant <jordan.lavenant@etu.univ-orleans.fr>
2024-09-16 09:48:08 +0200

    update models

--- GestionProduits/Produits/admin.py
+++ GestionProduits/Produits/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Product, Status
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'price', 'date', 'status')
+    search_fields = ('name', 'price')
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'libelle')
+    search_fields = ('libelle',)
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Status, StatusAdmin)
--- GestionProduits/Produits/views.py
+++ GestionProduits/Produits/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def products_list(request):
+    prdcts = Product.objects.all()
+    return render(
+        request,
+        'products.html',
+        {'products': prdcts}
+    )
